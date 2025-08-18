4344f451363f37ca717340481034acb50c0db63f
safrazchoudhury1 <safrazchoudhury02@gmail.com>
2025-08-17 22:17:14 -0700

    Add LIS algorithm solution (lis.py) (2025-08-18)

---
+++ coding/2025-08-18-lis/lis.py
@@ -0,0 +1,24 @@
+# Longest Increasing Subsequence (LIS) algorithm - dynamic programming with binary search
+import bisect
+
+
+def length_of_lis(nums):
+    """
+    Compute the length of the longest strictly increasing subsequence.
+    """
+    subseq = []
+    for x in nums:
+        # Find insertion position of x in the current subsequence
+        idx = bisect.bisect_left(subseq, x)
+        if idx == len(subseq):
+            subseq.append(x)
+        else:
+            subseq[idx] = x
+    return len(subseq)
+
+
+if __name__ == "__main__":
+    import sys
+    # Read integers from standard input
+    data = [int(x) for x in sys.stdin.read().split()]
+    print(length_of_lis(data))
